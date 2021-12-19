8fe79e4965010c8ae582637fc4edcba776da50f8
iwfan <i.wangfancn@gmail.com>
2021-12-19 21:11:02 +0800

    update extension

--- src/utils/opener.ts
+++ src/utils/opener.ts
@@ -5,7 +5,7 @@ export const openInBrowser = async (url: string) => {
 }
 
 export const urlBuilder = (prefix: string, text: string) => {
-  return /^https?:\/\//g.test(text) ? text : `${prefix}${text}`
+  return /^https?:\/\//g.test(text) ? text : `${prefix}${encodeURIComponent(text)}`
 }
 
 export const GoogleSearchOpner = async (text: string) => {
@@ -17,7 +17,7 @@ export const GithubSearchOpner = async (text: string) => {
 }
 
 export const GithubRepoOpner = async (text: string) => {
-  await openInBrowser(urlBuilder(`https://github.com/`, text))
+  await openInBrowser(`https://github.com/${text}`)
 }
 
 export const DeeplOpner = async (text: string) => {
